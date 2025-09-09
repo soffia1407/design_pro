7c0bca717fe4901e648488e0a061396d51d397e0
soffia1407 <sff140705@gmail.com>
2025-09-10 03:48:53 +0700

    add logout + profile

--- main/views.py
+++ main/views.py
@@ -2,14 +2,21 @@ from django.contrib import messages
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from .forms import RegisterForm, LoginForm
+from django.contrib.auth import logout
+
+def logout_view(request):
+    logout(request)
+    messages.success(request, 'Вы успешно вышли из системы.')
+    return redirect('home')
 
 def register_view(request):
     if request.method == 'POST':
         form = RegisterForm(request.POST)
         if form.is_valid():
-            form.save()
-            messages.success(request, 'Регистрация прошла успешно! Теперь вы можете войти.')
-            return redirect('login')
+            user = form.save()
+            login(request, user)
+            messages.success(request, 'Регистрация прошла успешно! Добро пожаловать в личный кабинет.')
+            return redirect('profile')
     else:
         form = RegisterForm()
     return render(request, 'register.html', {'form': form})
@@ -23,8 +30,8 @@ def login_view(request):
             user = authenticate(request, username=username, password=password)
             if user is not None:
                 login(request, user)
-                messages.success(request, 'Добро пожаловать!')
-                return redirect('home')
+                messages.success(request, 'Добро пожаловать в личный кабинет!')
+                return redirect('profile')
             else:
                 messages.error(request, 'Неверное имя пользователя или пароль.')
     else:
@@ -33,3 +40,9 @@ def login_view(request):
 
 def home_view(request):
     return render(request, 'home.html')
+
+def profile_view(request):
+    if not request.user.is_authenticated:
+        messages.error(request, 'Пожалуйста, войдите в систему.')
+        return redirect('login')
+    return render(request, 'profile.html')
